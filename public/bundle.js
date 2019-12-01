db3e83ae57c837f441f8db409cbb346c3919791a
mailer58 <50555861+mailer58@users.noreply.github.com>
2019-12-01 20:56:49 +0300

    Add fix

--- public/bundle.js
+++ public/bundle.js
@@ -97,8 +97,8 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createBoardTemplate", function() { return createBoardTemplate; });
 const createBoardTemplate = () => {
-    return (
-      `<section class="board container">
+  return (
+    `<section class="board container">
         <div class="board__filter-list">
           <a href="#" class="board__filter">SORT BY DEFAULT</a>
           <a href="#" class="board__filter">SORT BY DATE up</a>
@@ -106,8 +106,9 @@ const createBoardTemplate = () => {
         </div>
         <div class="board__tasks"></div>
       </section>`
-    );
-  };
+  );
+};
+
 
 /***/ }),
 
@@ -115,12 +116,15 @@ const createBoardTemplate = () => {
 /*!**********************************!*\
   !*** ./src/components/filter.js ***!
   \**********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! exports provided: createFilterTemplate */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createFilterTemplate", function() { return createFilterTemplate; });
 const createFilterTemplate = () => {
-    return (
-      `<section class="main__filter filter container">
+  return (
+    `<section class="main__filter filter container">
         <input
           type="radio"
           id="filter__all"
@@ -188,9 +192,9 @@ const createFilterTemplate = () => {
           >Archive <span class="filter__archive-count">115</span></label
         >
       </section>`
-    );
-  };
-  
+  );
+};
+
 
 /***/ }),
 
@@ -205,10 +209,219 @@ const createFilterTemplate = () => {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createLoadMoreButtonTemplate", function() { return createLoadMoreButtonTemplate; });
 const createLoadMoreButtonTemplate = () => {
-    return (
-      `<button class="load-more" type="button">load more</button>`
-    );
-  };
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
+
+
+/***/ }),
+
+/***/ "./src/components/task-edit.js":
+/*!*************************************!*\
+  !*** ./src/components/task-edit.js ***!
+  \*************************************/
+/*! exports provided: createTaskEditTemplate */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createTaskEditTemplate", function() { return createTaskEditTemplate; });
+const createTaskEditTemplate = () => {
+  return (
+    `<article class="card card--edit card--yellow card--repeat">
+      <form class="card__form" method="get">
+        <div class="card__inner">
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+  
+          <div class="card__textarea-wrap">
+            <label>
+              <textarea class="card__text" placeholder="Start typing your text here..." name="text">Here is a card with filled data</textarea>
+            </label>
+          </div>
+  
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                <button class="card__date-deadline-toggle" type="button">
+                  date: <span class="card__date-status">yes</span>
+                </button>
+  
+                <fieldset class="card__date-deadline">
+                  <label class="card__input-deadline-wrap">
+                    <input class="card__date" type="text" placeholder="" name="date" value="23 September 11:15 PM">
+                  </label>
+                </fieldset>
+  
+                <button class="card__repeat-toggle" type="button">
+                  repeat:<span class="card__repeat-status">yes</span>
+                </button>
+  
+                <fieldset class="card__repeat-days">
+                  <div class="card__repeat-days-inner">
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-mo-4" name="repeat" value="mo">
+                    <label class="card__repeat-day" for="repeat-mo-4">mo</label>
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-tu-4" name="repeat" value="tu" checked="">
+                    <label class="card__repeat-day" for="repeat-tu-4">tu</label>
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-we-4" name="repeat" value="we">
+                    <label class="card__repeat-day" for="repeat-we-4">we</label>
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-th-4" name="repeat" value="th">
+                    <label class="card__repeat-day" for="repeat-th-4">th</label>
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-fr-4" name="repeat" value="fr" checked="">
+                    <label class="card__repeat-day" for="repeat-fr-4">fr</label>
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" name="repeat" value="sa" id="repeat-sa-4">
+                    <label class="card__repeat-day" for="repeat-sa-4">sa</label>
+                    <input class="visually-hidden card__repeat-day-input" type="checkbox" id="repeat-su-4" name="repeat" value="su" checked="">
+                    <label class="card__repeat-day" for="repeat-su-4">su</label>
+                  </div>
+                </fieldset>
+              </div>
+  
+              <div class="card__hashtag">
+                <div class="card__hashtag-list">
+                  <span class="card__hashtag-inner">
+                    <input type="hidden" name="hashtag" value="repeat" class="card__hashtag-hidden-input">
+                    <p class="card__hashtag-name">
+                      #repeat
+                    </p>
+                    <button type="button" class="card__hashtag-delete">
+                      delete
+                    </button>
+                  </span>
+  
+                  <span class="card__hashtag-inner">
+                    <input type="hidden" name="hashtag" value="repeat" class="card__hashtag-hidden-input">
+                    <p class="card__hashtag-name">
+                      #cinema
+                    </p>
+                    <button type="button" class="card__hashtag-delete">
+                      delete
+                    </button>
+                  </span>
+  
+                  <span class="card__hashtag-inner">
+                    <input type="hidden" name="hashtag" value="repeat" class="card__hashtag-hidden-input">
+                    <p class="card__hashtag-name">
+                      #entertaiment
+                    </p>
+                    <button type="button" class="card__hashtag-delete">
+                      delete
+                    </button>
+                  </span>
+                </div>
+  
+                <label>
+                  <input type="text" class="card__hashtag-input" name="hashtag-input" placeholder="Type new hashtag here">
+                </label>
+              </div>
+            </div>
+  
+            <div class="card__colors-inner">
+              <h3 class="card__colors-title">Color</h3>
+              <div class="card__colors-wrap">
+                <input type="radio" id="color-black-4" class="card__color-input card__color-input--black visually-hidden" name="color" value="black">
+                <label for="color-black-4" class="card__color card__color--black">black</label>
+                <input type="radio" id="color-yellow-4" class="card__color-input card__color-input--yellow visually-hidden" name="color" value="yellow" checked="">
+                <label for="color-yellow-4" class="card__color card__color--yellow">yellow</label>
+                <input type="radio" id="color-blue-4" class="card__color-input card__color-input--blue visually-hidden" name="color" value="blue">
+                <label for="color-blue-4" class="card__color card__color--blue">blue</label>
+                <input type="radio" id="color-green-4" class="card__color-input card__color-input--green visually-hidden" name="color" value="green">
+                <label for="color-green-4" class="card__color card__color--green">green</label>
+                <input type="radio" id="color-pink-4" class="card__color-input card__color-input--pink visually-hidden" name="color" value="pink">
+                <label for="color-pink-4" class="card__color card__color--pink">pink</label>
+              </div>
+            </div>
+          </div>
+  
+          <div class="card__status-btns">
+            <button class="card__save" type="submit">save</button>
+            <button class="card__delete" type="button">delete</button>
+          </div>
+        </div>
+      </form>
+    </article>`
+  );
+};
+
+
+/***/ }),
+
+/***/ "./src/components/task.js":
+/*!********************************!*\
+  !*** ./src/components/task.js ***!
+  \********************************/
+/*! exports provided: createTaskTemplate */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createTaskTemplate", function() { return createTaskTemplate; });
+const createTaskTemplate = () => {
+  return `<article class="card card--black">
+      <div class="card__form">
+        <div class="card__inner">
+          <div class="card__control">
+            <button type="button" class="card__btn card__btn--edit">
+              edit
+            </button>
+            <button type="button" class="card__btn card__btn--archive">
+              archive
+            </button>
+            <button
+              type="button"
+              class="card__btn card__btn--favorites card__btn--disabled"
+            >
+              favorites
+            </button>
+          </div>
+          <div class="card__color-bar">
+            <svg class="card__color-bar-wave" width="100%" height="10">
+              <use xlink:href="#wave"></use>
+            </svg>
+          </div>
+          <div class="card__textarea-wrap">
+            <p class="card__text">Example default task with default color.</p>
+          </div>
+          <div class="card__settings">
+            <div class="card__details">
+              <div class="card__dates">
+                <div class="card__date-deadline">
+                  <p class="card__input-deadline-wrap">
+                    <span class="card__date">23 September</span>
+                    <span class="card__time">11:15 PM</span>
+                  </p>
+                </div>
+              </div>
+              <div class="card__hashtag">
+                <div class="card__hashtag-list">
+                  <span class="card__hashtag-inner">
+                    <span class="card__hashtag-name">
+                      #todo
+                    </span>
+                  </span>
+                  <span class="card__hashtag-inner">
+                    <span class="card__hashtag-name">
+                      #personal
+                    </span>
+                  </span>
+                  <span class="card__hashtag-inner">
+                    <span class="card__hashtag-name">
+                      #important
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>`;
+};
+
 
 /***/ }),
 
@@ -223,34 +436,36 @@ const createLoadMoreButtonTemplate = () => {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_board_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/board.js */ "./src/components/board.js");
 /* harmony import */ var _components_filter_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/filter.js */ "./src/components/filter.js");
-/* harmony import */ var _components_filter_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_components_filter_js__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _components_load_more_button_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/load-more-button.js */ "./src/components/load-more-button.js");
+/* harmony import */ var _components_task_edit__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/task-edit */ "./src/components/task-edit.js");
+/* harmony import */ var _components_task_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/task.js */ "./src/components/task.js");
 
 
 
-//import createSiteMenuTemplate from './components/site-menu.js';
+
+
+
+
 
 const TASK_COUNT = 3;
 const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
-//render(siteHeaderElement, createSiteMenuTemplate(), `beforeend`);
 
-render(siteMainElement, _components_filter_js__WEBPACK_IMPORTED_MODULE_1___default()(), `beforeend`);
-render(siteMainElement, Object(_components_board_js__WEBPACK_IMPORTED_MODULE_0__["default"])(), `beforeend`);
+render(siteMainElement, Object(_components_filter_js__WEBPACK_IMPORTED_MODULE_1__["createFilterTemplate"])(), `beforeend`);
+render(siteMainElement, Object(_components_board_js__WEBPACK_IMPORTED_MODULE_0__["createBoardTemplate"])(), `beforeend`);
 
 const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-render(taskListElement, createTaskEditTemplate(), `afterbegin`);
+render(taskListElement, Object(_components_task_edit__WEBPACK_IMPORTED_MODULE_3__["createTaskEditTemplate"])(), `afterbegin`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), `beforeend`);
+  render(taskListElement, Object(_components_task_js__WEBPACK_IMPORTED_MODULE_4__["createTaskTemplate"])(), `beforeend`);
 }
 
-render(boardElement, Object(_components_load_more_button_js__WEBPACK_IMPORTED_MODULE_2__["default"])(), `beforeend`);
+render(boardElement, Object(_components_load_more_button_js__WEBPACK_IMPORTED_MODULE_2__["createLoadMoreButtonTemplate"])(), `beforeend`);
 
 
 /***/ })
